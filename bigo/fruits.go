74783a3ddcc06b091d83fd4d8efd6847c56a81e9
Mohamed Asheik <mohamedasheik@gmail.com>
2021-11-30 19:09:21 +0530

    Update fruits.go

--- bigo/fruits.go
+++ bigo/fruits.go
@@ -2,29 +2,42 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"strings"
 	"time"
 )
 
 func main() {
+	// Tell the system to use cpus available, let us see if it means ot anything
+	// without using goroutines
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	strslice := make([]string, 1, 1)
 	start := time.Now()
 	strArray := [5]string{"apple", "orange", "pinapple", "mango", "banana"}
 
-	filler(10000000, strslice, "banana")
+	strslice = filler(10000, strslice, "banana")
 
 	for _, v := range strArray {
 		if strings.Compare("mango", v) == 0 {
 			fmt.Printf("Banana found \n")
 		}
 	}
+
+	for _, v := range strslice {
+		if strings.Compare("banana", v) == 0 {
+			fmt.Printf("Banana found \n")
+		}
+	}
 	end := time.Since(start)
+	fmt.Printf("len of strslice: %d\n", len(strslice))
 	fmt.Println("Took ", end, "to complete")
 }
 
 // filler
-func filler(n int, strslice []string, fillerstr string) {
+func filler(n int, strslice []string, fillerstr string) []string {
 	for i := 0; i < n; i++ {
 		strslice = append(strslice, fillerstr)
 	}
+	return strslice
 }
