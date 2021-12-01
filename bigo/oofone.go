ab1af55781d959a120e0b25db3442a7337bc967d
Mohamed Asheik <mohamedasheik@gmail.com>
2021-12-01 12:23:02 +0530

    rename and examples

--- bigo/oofone.go
+++ bigo/oofone.go
@@ -11,10 +11,21 @@ import (
 func main() {
 	arr := [...]int{1, 2, 3, 4, 5, 6, 7, 9, 9, 10}
 	printFistTwoElements(arr)
+	evenorOdd(7)
 
 }
 
+// o(1)
 func printFistTwoElements(arr [10]int) {
 	fmt.Println(arr[0])
 	fmt.Println(arr[1])
+}
+
+// o(1)
+func evenorOdd(num int) {
+	if num % 2 {
+		fmt.Println("even")
+	} else {
+		fmt.Println("odd")
+	}
 }
