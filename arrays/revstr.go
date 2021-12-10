3836161993098df7b0efda24259308309cd0a56e
Mohamed Asheik <mohamedasheik@gmail.com>
2021-12-11 00:32:10 +0530

    string reverse using rune for unicode

--- arrays/revstr.go
+++ arrays/revstr.go
@@ -21,14 +21,28 @@ convert back to string before returning. works only with non unicode type string
 */
 func main() {
 	s := "test123 4"
-	fmt.Println(reverseString(s))
+	fmt.Println(reverseString1(s))
+	fmt.Println(reverseString2(s))
 }
 
-func reverseString(s string) string {
-	a := []byte(s)
+func reverseString1(s string) string {
+	b := []byte(s)
 	for i, j := 0, len(s)-1; i < j; i++ {
-		a[i], a[j] = a[j], a[i]
+		b[i], b[j] = b[j], b[i]
 		j--
 	}
-	return string(a)
+	return string(b)
+}
+
+/* String Reverse using rune *
+   Rune is to hanle not only chars but also unicode chars
+   rune is considered int32 equavalent */
+func reverseString2(s string) string {
+	r := []rune(s) // convert string to rune
+	for i, j := 0, len(s)-1; i < j; i++ {
+		r[i], r[j] = r[j], r[i] // Swap
+		j--
+	}
+	return string(r) // convert back to string
+
 }
