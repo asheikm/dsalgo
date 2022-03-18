20b07f090929679424dbed7959f3f9d2902257b6
Mohamed Asheik <mohamedasheik@gmail.com>
2022-03-18 11:58:43 +0530

    fmtchange

--- gopatterns/fungen.go
+++ gopatterns/fungen.go
@@ -1,36 +1,36 @@
 package main
+
 import "fmt"
 
 func main() {
-    numofReceiveGoRoutines := 2 // configurable
-    funCh := make(chan func())
-    
-    // receive funs and exxecute
-    for i := 0; i < numofReceiveGoRoutines; i++ {
-        go receiveFuncs(funCh)
-    }
-    
-    funcGenerator(funCh, sendFunction)
+	numofReceiveGoRoutines := 2 // configurable
+	funCh := make(chan func())
+
+	// receive funs and exxecute
+	for i := 0; i < numofReceiveGoRoutines; i++ {
+		go receiveFuncs(funCh)
+	}
+
+	funcGenerator(funCh, sendFunction)
 }
 
 func sendFunction() {
-    fmt.Println("welcome")
+	fmt.Println("welcome")
 }
 
-
-// Function generator or provider which sends funs 
+// Function generator or provider which sends funs
 func funcGenerator(funCh chan func(), f func()) {
-    for {
-        funCh  <-  f
-    }   
+	for {
+		funCh <- f
+	}
 }
 
 // func exxecutor
 func receiveFuncs(funCh chan func()) {
-        for {
-            e := <-funCh
-            e()
-        }
+	for {
+		e := <-funCh
+		e()
+	}
 }
 
 // I have a input provider that sends me funcs that needs to be executed.
