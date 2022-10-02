b6e26b9f48fca125a15c4910c895bff6176a9de9
Mohamed Asheik <33277633+asheikm@users.noreply.github.com>
2022-10-02 09:06:49 -0700

    Update rotatarray.go

--- arrays/rotatarray.go
+++ arrays/rotatarray.go
@@ -7,8 +7,28 @@ import (
 func main() {
 	inputArray := [5]int{1, 2, 3, 4, 5}
 	k := 2
+	// sol1
 	out := rotateArray(inputArray, k)
 	fmt.Println(out)
+	// sol2
+	rotate(inputArray, k)
+	fmt.Println(inputArray)
+}
+
+// This one bit improved
+func rotate(nums []int, k int)  {
+    if len(nums) == 1 {
+        return
+    }
+    if k > len(nums) {
+        k = k % len(nums)
+    }
+    lastHalf := nums[len(nums)-k:]
+	firstHalf := nums[:len(nums)-k]
+    combined := append(lastHalf, firstHalf...)
+    for i := range nums {
+        nums[i] = combined[i]
+    }
 }
 
 // This solution can be improved
